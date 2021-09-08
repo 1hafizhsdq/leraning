b16360814ad576e2e6e57c3e32310f3bf77e384a
dev kominfosda <dev.kominfosda@gmail.com>
2021-09-08 13:39:05 +0700

    row column

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Tutorial Text"),
-        ),
-        body: Center(
-          child: Container(
-              color: Colors.lightBlue,
-              width: 150,
-              height: 100,
-              child: Text(
-                "Saya sedang melatih kemampuan flutter saya.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.lightGreen[900],
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20),
-              )),
+        appBar: AppBar(title: Text("Tuttorial Row Column"),),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Text 1"),
+            Text("Text 2"),
+            Text("Text 3"),
+            Row(
+              children: [
+                Text("Text 4"),
+                Text("Text 5"),
+                Text("Text 6"),
+              ],
+            )
+          ],
         ),
       ),
     );
