2220a88dcef6062225c34fb8221efbed0a624fe7
dev kominfosda <dev.kominfosda@gmail.com>
2021-09-10 09:56:31 +0700

    image widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,111 +4,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("stack & align")),
-        body: Stack(
-          children: [
-            Column(
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: Row(
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          color: Colors.white,
-                        ),
-                      ),
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          color: Colors.black12,
-                        ),
-                      ),
-                    ],
+        appBar: AppBar(
+          title: Text("Image widget"),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.amber,
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(3),
+            child: Image(
+              image: AssetImage(
+                  "images/minion.jpg"),
+                  fit: BoxFit.contain,
                   ),
-                ),
-                Flexible(
-                  flex: 1,
-                  child: Row(
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          color: Colors.black12,
-                        ),
-                      ),
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            ListView(
-              children: [
-                Column(
-                  children: [
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text(
-                            "Ini adalah text yang ada di tengah dari stack",
-                            style: TextStyle(fontSize: 30))),
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text(
-                            "Ini adalah text yang ada di tengah dari stack",
-                            style: TextStyle(fontSize: 30))),
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text(
-                            "Ini adalah text yang ada di tengah dari stack",
-                            style: TextStyle(fontSize: 30))),
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text(
-                            "Ini adalah text yang ada di tengah dari stack",
-                            style: TextStyle(fontSize: 30))),
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text(
-                            "Ini adalah text yang ada di tengah dari stack",
-                            style: TextStyle(fontSize: 30))),
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text(
-                            "Ini adalah text yang ada di tengah dari stack",
-                            style: TextStyle(fontSize: 30))),
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text(
-                            "Ini adalah text yang ada di tengah dari stack",
-                            style: TextStyle(fontSize: 30))),
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text(
-                            "Ini adalah text yang ada di tengah dari stack",
-                            style: TextStyle(fontSize: 30))),
-                  ],
-                )
-              ],
-            ),
-            Align(
-              alignment: Alignment(0,0.97),
-              child: RaisedButton(
-                child: Text("My Button"),
-                color: Colors.amber,
-                onPressed: () {},
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
