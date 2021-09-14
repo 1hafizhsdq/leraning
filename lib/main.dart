18416c1343631222d14a1d711c1a2595906c1e65
dev kominfosda <dev.kominfosda@gmail.com>
2021-09-14 11:54:55 +0700

    Dragable

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,112 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color color1 = Colors.red;
+  Color color2 = Colors.amber;
+  Color targetColor;
+  bool isAccepted = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Image widget"),
+          title: Text("Dragablex"),
         ),
-        body: Center(
-          child: Container(
-            color: Colors.amber,
-            width: 200,
-            height: 200,
-            padding: EdgeInsets.all(3),
-            child: Image(
-              image: AssetImage(
-                  "images/minion.jpg"),
-                  fit: BoxFit.contain,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Draggable(
+                  data: color1,
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
                   ),
-          ),
+                  childWhenDragging: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: Colors.grey,
+                      shape: StadiumBorder()
+                    ),
+                  ),
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1.withOpacity(0.7),
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                ),
+                Draggable(
+                  data: color2,
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(color: Colors.grey, shape: StadiumBorder()),
+                  ),
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2.withOpacity(0.7),
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            DragTarget(
+              onWillAccept: (value) => true,
+              onAccept: (value) {isAccepted = true; targetColor = value;},
+              builder: (context, candidates, rejected) {
+                return (isAccepted) ? SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: targetColor,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ) :
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: Colors.grey,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  );
+              },
+            ),
+          ],
         ),
       ),
     );
