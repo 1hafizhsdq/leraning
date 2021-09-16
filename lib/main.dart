8ffe62383809d31963752d1b869d5d619bfa15e3
dev kominfosda <dev.kominfosda@gmail.com>
2021-09-16 14:51:01 +0700

    Positioned, FloatingActionButton, LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,217 @@
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:learning/login_page.dart';
 import 'package:learning/main_page.dart';
 import 'package:learning/second_page.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
-class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Text Field"),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.lightBlue[50],
-                  filled: true,
-                  icon: Icon(Icons.android),
-                  prefixIcon: Icon(Icons.android),
-                  prefixText: "Nama",
-                  prefixStyle: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
-                  labelText: "Nama Lengkap",
-                  hintText: "Nama Lengkap",
-                  hintStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.w600),
-                  // suffix: Container(width: 100, height: 100, color: Colors.red,),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
-                ),
-                maxLength: 5,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                controller: controller,
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  double getSmallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  double getBigDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEEEEEE),
+      body: Stack(
+        children: [
+          Positioned(
+            right: -getSmallDiameter(context) / 3,
+            top: -getSmallDiameter(context) / 3,
+            child: Container(
+              width: getSmallDiameter(context),
+              height: getSmallDiameter(context),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                    colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter),
               ),
-              Text(controller.text),
-            ],
+            ),
           ),
-        ),
+          Positioned(
+            left: -getBigDiameter(context) / 4,
+            top: -getBigDiameter(context) / 4,
+            child: Container(
+              child: Center(
+                child: Text(
+                  "Aplikasiku",
+                  style: TextStyle(
+                      fontFamily: "Pacifico",
+                      fontSize: 30,
+                      color: Colors.white),
+                ),
+              ),
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                    colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter),
+              ),
+            ),
+          ),
+          Positioned(
+            right: -getBigDiameter(context) / 2,
+            bottom: -getBigDiameter(context) / 2,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Color(0XFFF3E9EE)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
+                  child: Column(children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.email,
+                            color: Color(0xFFFF4891),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFFF4891))),
+                          labelText: "Email : ",
+                          labelStyle: TextStyle(color: Color(0xFFFF4891))),
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.vpn_key,
+                            color: Color(0xFFFF4891),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFFF4891))),
+                          labelText: "Password : ",
+                          labelStyle: TextStyle(color: Color(0xFFFF4891))),
+                    ),
+                  ]),
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
+                        child: Text(
+                          "FORGOT PASSWORD?",
+                          style:
+                              TextStyle(color: Color(0xFFFF4891), fontSize: 18),
+                        ))),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: 40,
+                        child: Container(
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(20),
+                              splashColor: Colors.amber,
+                              onTap: () {},
+                              child: Center(
+                                child: Text(
+                                  "SIGN IN",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: LinearGradient(
+                                colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                              )),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        mini: true,
+                        elevation: 0,
+                        child: Image(
+                          image: AssetImage("assets/facebook.png"),
+                        ),
+                        onPressed: () {},
+                      ),
+                      FloatingActionButton(
+                        mini: true,
+                        elevation: 0,
+                        child: Image(
+                          image: AssetImage("assets/twitter.png"),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "DON'T HAVE AN ACCOUNT? ",
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "SIGN UP",
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Color(0xFFFF4891),
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
