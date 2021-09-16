5bafd89727669bf62df44f163157e043aec24b68
dev kominfosda <dev.kominfosda@gmail.com>
2021-09-16 08:42:05 +0700

    flexible layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,51 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: '/',
-      getPages: [
-        GetPage(name: '/', page: () => LoginPage()),
-        GetPage(
-            name: '/second',
-            page: () => SecondPage(),
-            transition: Transition.cupertino,),
-        GetPage(
-            name: '/main',
-            page: () => MainPage(),
-            transition: Transition.cupertino),
-      ],
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Flexible Layout"),),
+        body: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.red,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.purple,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                color: Colors.amber,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
